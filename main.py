ec4bbfc8c4881f673c40de9f0467e17e1700c871
ANORA\yoshuaraju <yoshuaraju@anorasolutions.com>
2026-03-19 05:07:13 +0530

    no message

--- main.py
+++ main.py
@@ -3,23 +3,4 @@ def out(name):
 
 print("yoshua")
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-print("Fixing Conflict")
-=======
-print("Fixing the conflict")
->>>>>>> temp
-=======
-print("Making a conflict")
->>>>>>> origin/test
-=======
-print("Fixed the confict")
->>>>>>> temp
-=======
-print("Creating Conflict")
->>>>>>> temp
-
-
-print("")
+print("Conflict")
